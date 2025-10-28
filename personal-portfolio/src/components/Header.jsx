d82de045667654f6bf9720052b0a8d76ad5450d4
Annette <annette.ngugi@student.moringaschool.com>
2025-10-28 17:03:43 +0300

    add home section

--- personal-portfolio/src/components/Header.jsx
+++ personal-portfolio/src/components/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
     <div className='Header'>
       <nav>
         <a href='#home'>Home</a>
-        <a href='#about'>Home</a>
-        <a href='#skills'>Home</a>
-        <a href='#projects'>Home</a>
-        <a href='#contacts'>Home</a>
-        <a href='#stats'>Home</a>
+        <a href='#about'>About</a>
+        <a href='#skills'>Skills</a>
+        <a href='#projects'>Projects</a>
+        <a href='#contacts'>Contacts</a>
+        <a href='#stats'>Stats</a>
       </nav>
     </div>
   )
--- personal-portfolio/src/components/Home.jsx
+++ personal-portfolio/src/components/Home.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <section id='home' className='home'>
+      <h2>
+        <p>Hi there ! I'm Annette Ngugi</p>
+        <code></code>
+        <a href='#about'>About me</a>
+        <a href='#skills'>Skills</a>
+        <a href='#projects'>Projects</a>
+      </h2>
+      </section>
     </div>
   )
 }
