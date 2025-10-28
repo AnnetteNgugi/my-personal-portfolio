24e45c51e550dbb74c97c8ca568d372a88163180
Annette <annette.ngugi@student.moringaschool.com>
2025-10-28 16:40:09 +0300

    add header with navigation links

--- personal-portfolio/src/components/Header.jsx
+++ personal-portfolio/src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='Header'>
+      <nav>
+        <a href='#home'>Home</a>
+        <a href='#about'>Home</a>
+        <a href='#skills'>Home</a>
+        <a href='#projects'>Home</a>
+        <a href='#contacts'>Home</a>
+        <a href='#stats'>Home</a>
+      </nav>
     </div>
   )
 }
